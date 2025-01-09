e640657f7bb49c07fbefbe7f967ed3f2d83c5b94
Omkar <omkarkhawaldar@gmail.com>
2025-01-09 22:47:30 +0530

    Working on lists

--- C# practice/Program.cs
+++ C# practice/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("generic lists in C#");
+
+List<string> studentList = new List<string>();
+
+for (int i = 0; i < 2; i++)
+{
+    studentList.Add(Console.ReadLine());
+}
+
+for (int i = 0; i < studentList.Count; i++)
+{
+    Console.WriteLine("The element at index " + i + " is " + studentList[i]);
+}
+
